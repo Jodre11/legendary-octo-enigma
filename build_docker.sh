e25ea93271b602bbeb102d160afe64b356aa39a9
Jodre11 <jodre11.public@gmail.com>
2025-06-17 14:48:32 +0100

    Update Docker build script to use BuildKit and upgrade Vite and dependencies in package.json and package-lock.json

--- build_docker.sh
+++ build_docker.sh
@@ -3,4 +3,4 @@
 DOCKER_TAG=${1:-my-app}
 DOCKER_DEFAULT_PLATFORM=${2:-linux/amd64}
 
-docker build --platform $DOCKER_DEFAULT_PLATFORM -t $DOCKER_TAG .
+DOCKER_BUILDKIT=1 docker build --platform $DOCKER_DEFAULT_PLATFORM -t $DOCKER_TAG .
